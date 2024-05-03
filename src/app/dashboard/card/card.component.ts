b0c26269407e4acc4bcfc1684111387f237b99e3
Washington Ribeiro <washinuchiha@gmail.com>
2024-05-03 19:33:41 -0300

    feat: updating card likes and adding loading

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 export interface ICard {
   description: string;
@@ -10,29 +10,39 @@ export interface ICard {
 }
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent {
-
   constructor(private httpClient: HttpClient) {}
 
   @Input() card: ICard;
-  color: string = '#75e975';
+  color: string = "#75e975";
+  loading = false;
 
-  onLike(){
-    this.card.likes++;
+  onLike() {
+    this.loading = true;
+    this.httpClient.put(`/api/skills/${this.card.id}`, this.card).subscribe(
+      () => {
+        this.card.likes++;
 
-    if (this.card.likes >= 5) this.color = "#36bdf9";
-    if (this.card.likes > 10) this.color = "#f56464";
+        if (this.card.likes >= 5) this.color = "#36bdf9";
+        if (this.card.likes > 10) this.color = "#f5b364";
 
-    // TODO: incrementar o like, salvar via rest
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
   }
 
-  onShare(){
-    window.open('https://www.linkedin.com/in/washington-ribeiro-da-silva/', '_blank');
+  onShare() {
+    window.open(
+      "https://www.linkedin.com/in/washington-ribeiro-da-silva/",
+      "_blank"
+    );
   }
-
 }
