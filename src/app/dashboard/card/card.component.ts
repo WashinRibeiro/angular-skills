e3ea689973d0fe49aeab3d608efe1ddf6da0a6f6
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2024-05-03 16:55:35 -0300

    feat: structuring and stylizing cards

--- src/app/dashboard/card/card.component.ts
+++ src/app/dashboard/card/card.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+export interface ICard {
+  description: string;
+  id: number;
+  likes: number;
+  name: string;
+  pic: string;
+}
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
 
-  @Input() card;
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {
-  }
+  @Input() card: ICard;
+  color: string = '#75e975';
 
-  onLike(card: any){
+  onLike(){
+    this.card.likes++;
+
+    if (this.card.likes >= 5) this.color = "#36bdf9";
+    if (this.card.likes > 10) this.color = "#f56464";
+
     // TODO: incrementar o like, salvar via rest
   }
 
-  onShare(card: any){
-    // TODO: abrir o link do seu linkedin
+  onShare(){
+    window.open('https://www.linkedin.com/in/washington-ribeiro-da-silva/', '_blank');
   }
 
 }
