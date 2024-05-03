71214b4c0066db8ff7f09b73d7fea0a15955c643
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2024-05-03 16:57:32 -0300

    feat: structuring and responsiveness of the project

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ICard } from "./card/card.component";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"],
 })
 export class DashboardComponent implements OnInit {
+  cards: Array<ICard>;
+  loading: boolean = true;
 
-  cards: Array<any>;
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
-    this.httpClient.get('/api/skills').subscribe((ret: Array<any>) => this.cards = ret);
+    this.httpClient.get("/api/skills").subscribe((ret: Array<ICard>) => {
+      this.cards = ret;
+      this.loading = false;
+    });
   }
-
 }
