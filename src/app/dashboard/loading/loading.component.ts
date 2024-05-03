a4a7e70cd8bccfb41ee10c023e7597bfed9ca34d
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2024-05-03 16:54:53 -0300

    feat: creating the loading component

---
+++ src/app/dashboard/loading/loading.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-loading",
+  templateUrl: "./loading.component.html",
+  styleUrls: ["./loading.component.css"],
+  encapsulation: ViewEncapsulation.None,
+})
+export class LoadingComponent {}
